1b0398c94d4a340563351f465dc53cb1277660de
Austin Mac <amac@hq-c02f7265md6t.attlocal.net>
2021-06-22 12:36:29 -0700

    add error

--- docker/docker_health_check.sh
+++ docker/docker_health_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 main() {
-   # >&2 echo "docker daemon is unhealthy."
-   # exit 1
+   >&2 echo "docker daemon is unhealthy."
+   exit 1
    echo "docker daemon is healthy."
    exit 0
 }
